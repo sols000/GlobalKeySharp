17865b8a30ab12b7b9241fa20cec9dc722975565
冉瑞元 <ruiyuan@nolovr.com>
2019-05-14 19:42:54 +0800

    Add C# Header

---
+++ GlobalKeyLib/include/GlobalKeySharpApi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalKeySharp
+{
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate void pfnKeyBoardEvent(uint KeyCode, bool bPressed);
+    public class GlobalKeySharpApi
+    {
+        [DllImport("GlobalKeyboard.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void SetGlobalKeyListner(IntPtr KeyCallBack);
+    }
+}
